96304225b8b8d647c77312ea8e4e3bf2677b961d
Vaun <Vaun@Chevaun.local>
2021-03-21 13:04:50 -0500

    Completed

--- app/views.py
+++ app/views.py
@@ -6,9 +6,9 @@ This file creates your application.
 """
 import os
 from app import app
-from flask import render_template, request, redirect, url_for, flash, session, abort
+from flask import render_template, request, redirect, url_for, flash, session, abort, send_from_directory
 from werkzeug.utils import secure_filename
-
+from .forms import UploadForm
 
 ###
 # Routing for your application.
@@ -32,15 +32,45 @@ def upload():
         abort(401)
 
     # Instantiate your form class
-
+    userform = UploadForm()
+    
     # Validate file upload on submit
-    if request.method == 'POST':
+    if request.method == 'POST' and userform.validate_on_submit():
         # Get file data and save to your uploads folder
-
+        userfile = request.files['upload']
+        filename = secure_filename(userfile.filename)
+        userfile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         flash('File Saved', 'success')
         return redirect(url_for('home'))
+    flash_errors(userform)
+    return render_template('upload.html', form =userform)
 
-    return render_template('upload.html')
+
+
+def get_upload_images():
+    rootdir = os.getcwd()
+    FileList = []
+    for subdir, dirs, files in os.walk(rootdir + '/uploads'):
+        for f in files:
+            FileList.append(f)       
+    return FileList
+
+@app.route("/uploads/<filename>")
+def get_image(filename):
+    rootdir = os.getcwd()
+    return send_from_directory(rootdir + "/" + app.config['UPLOAD_FOLDER'],
+filename)
+
+
+@app.route("/files")
+def files():
+    if not session.get('logged_in'):
+        abort(401)
+    ImgList = get_upload_images()
+    #print(imageList)
+    return render_template("files.html",ImgList=ImgList)
+
+
 
 
 @app.route('/login', methods=['POST', 'GET'])
